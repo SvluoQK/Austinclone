ec9af4fc3d0c6c729b8af39fd3ab6f56f0d2ee9d
Svluo <woshisvluo@gmail.com>
2022-02-05 23:00:43 -0500

    added a user

---
+++ leanone/src/main/java/com/hao/leanone/pojo/User.java
@@ -0,0 +1,21 @@
+package com.hao.leanone.pojo;
+
+import lombok.Data;
+
+/**
+ * @author Hao Luo
+ * @Date 2022
+ * @description
+ */
+
+
+
+@Data
+public class User {
+    private int ID;
+    private String name;
+    private String Address;
+
+
+
+}
