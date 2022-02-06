7edc94b3da8d5b08998a4314b0e62e68a54d19ad
Svluo <woshisvluo@gmail.com>
2022-02-05 22:43:21 -0500

    just a lean demo

---
+++ leanone/src/main/java/com/hao/leanone/LeanoneApplication.java
@@ -0,0 +1,13 @@
+package com.hao.leanone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeanoneApplication.class, args);
+    }
+
+}
---
+++ leanone/src/test/java/com/hao/leanone/LeanoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.leanone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
